54165bb0118f682bea68b2071a6659aebb77144a
Alex Makunin <am60@sanger.ac.uk>
2023-03-29 11:00:10 +0100

    bump version

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ install_requires = \
 
 setup_kwargs = {
     'name': 'anospp-analysis',
-    'version': '0.1.0',
+    'version': '0.1.1',
     'description': 'ANOSPP data analysis',
     'long_description': 'Anopheles species identification with amplicon sequencing - data analysis',
     'author': 'Alex Makunin',
     'author_email': 'am60@sanger.ac.uk',
     'maintainer': 'None',
     'maintainer_email': 'None',
-    'url': 'None',
+    'url': 'https://github.com/malariagen/anospp-analysis/',
     'packages': packages,
     'package_data': package_data,
     'install_requires': install_requires,
