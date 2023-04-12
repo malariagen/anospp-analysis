79ca7feef1a46dc9257020171212d7a9f1abbf8f
marilou bodde <mariloubodde@gmail.com>
2023-04-12 16:24:45 +0100

    construct kmer table for unique sequences

--- anospp_analysis/nn.py
+++ anospp_analysis/nn.py
@@ -5,10 +5,31 @@ import matplotlib.pyplot as plt
 from collections import OrderedDict
 import os
 import argparse
+import itertools
 
 from .util import *
 
+def prep_mosquito_haps(hap_df):
+    '''
+    prepare mosquito haplotype dataframe
+    remove plasmodium haplotypes
+    change targets to integers
+    returns haplotype dataframe
+    '''
+
+    logging.info('Prepare mosquito haplotypes')
+
+    hap_df = hap_df.astype({'target': str})
+    hap_df = hap_df[hap_df.target.isin(MOSQ_TARGETS)]
+    hap_df = hap_df.astype({'target': int})
+    hap_df.reset_index(inplace=True, drop=True)
+
+    return(hap_df)
+
 def prep_reference_index(reference_dn):
+    '''
+    Read in standardised reference index files from database (currently directory)
+    '''
 
     logging.info(f'Importing reference index {reference_dn}')
 
@@ -52,7 +73,47 @@ def prep_reference_index(reference_dn):
 
     return(ref_hap_df, af_c, af_i, af_f)
 
+def construct_kmer_dict(k):
+    '''
+    construct a k-mer dict
+    associating each unique k-mer of length k with a unique non-negative integer <4**k
+    bases are written in capitals
+    returns a dictionary
+    '''
+    labels = []
+    for i in itertools.product('ACGT', repeat=k):
+        labels.append(''.join(i))
+    kmerdict = dict(zip(labels, np.arange(4**k)))
+    return(kmerdict)    
+
+def construct_unique_kmer_table(hap_df, k):
+    '''
+    constructs a k-mer table of dimensions n_amp * maxallele * 4**k
+    represting the k-mer table of each unique sequence in the dataframe
+    maxallele is the maximum number of unique sequences per target
+    n_amp is the number of mosquito targets
+    input: k=k (length of k-mers), hap_df=dataframe with haplotypes
+    output: k-mer table representing each unique haplotype in the hap dataframe
+    '''
+
+    logging.info('Translate unique sequences to k-mers')
+
+    maxallele = hap.groupby('target')['seqid'].nunique().max()
+    kmerdict = construct_kmer_dict(k)
     
+    uniqueseq = hap[['seqid', 'consensus']].drop_duplicates()
+
+    table = np.zeros((len(MOSQ_TARGETS), maxallele, 4**k), dtype='int')
+    for r in seq.index:
+        seqid = str.split(uniqueseq.loc[r,'seqid'], '-')
+        t, u = int(seqid[0]), int(seqid[1])
+        sq = uniqueseq.loc[r,'consensus']
+        for i in np.arange(len(sq)-(k-1)):
+            table[t,u,kmerdict[sq[i:i+k]]] += 1
+    return(table)
+
+
+
 
 def nn(args):
 
@@ -67,6 +128,8 @@ def nn(args):
     stats_df = prep_stats(args.stats)
 
     comb_stats_df = combine_stats(stats_df, hap_df, samples_df)
+    hap_df = prep_mosquito_haps(hap_df)
+    test_samples = comb_stats_df.loc[comb_stats_df.mosq_targets_recovered >= 10, 'sample_id'].values
 
     ref_hap_df, af_c, af_i, af_f = prep_reference_index(args.reference)
 
