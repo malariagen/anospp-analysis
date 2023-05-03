d04031ef814ff442096d3d4c2c4270c053ca4403
marilou bodde <mariloubodde@gmail.com>
2023-05-03 12:12:18 +0100

    prep data

--- anospp_analysis/vae.py
+++ anospp_analysis/vae.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 import os
 import argparse
-import itertools
 
 from util import *
-from nn import construct_kmer_dict, parse_seqids_series, construct_unique_kmer_table
+from nn import parse_seqids_series, construct_unique_kmer_table
+
+#Variables
+K = 8
 
 def prep_reference_index(reference_version, path_to_refversion):
     '''
@@ -22,10 +23,11 @@ def prep_reference_index(reference_version, path_to_refversion):
     assert os.path.isdir(reference_path), f'reference version {reference_version} does not \
         exist at {reference_path}'
 
-    assert os.path.isfile(f'{reference_path}/selection_criteria.tsv'), f'reference version \
+    assert os.path.isfile(f'{reference_path}/selection_criteria.txt'), f'reference version \
         {reference_version} at {reference_path} does not contain required \
-        selection_criteria.tsv file'
-    selection_criteria = pd.read_csv(f'{reference_path}/selection_criteria.tsv', sep='\t')
+        selection_criteria.txt file'
+    level, sgp, n_targets_str = open(f'{reference_path}/selection_criteria.txt').read().split('\t')
+    n_targets = int(n_targets_str)
 
     if os.path.isfile(f'{reference_path}/version.txt'):
         with open(f'{reference_path}/version.txt', 'r') as fn:
@@ -36,22 +38,7 @@ def prep_reference_index(reference_version, path_to_refversion):
                         at {reference_path}')
         version_name = 'unknown'
         
-    return(selection_criteria, version_name)
-
-def select_samples_from_file(comb_stats_df, hap_df, config_file):
-    
-    level, sgp, n_targets = open(config_file).read().split('\t')
-
-    return select_samples(comb_stats_df, hap_df, level, sgp, n_targets)
-
-def select_samples_from_df(comb_stats_df, hap_df, selection_criteria):
-    
-    #Unwrap selection criteria 
-    level = selection_criteria.loc[0,'level']
-    sgp = selection_criteria.loc[0,'sgp']
-    n_targets = int(selection_criteria.loc[0,'n_targets'])
-
-    return select_samples(comb_stats_df, hap_df, level, sgp, n_targets)
+    return level, sgp, n_targets, version_name
 
 def select_samples(comb_stats_df, hap_df, level, sgp, n_targets):
     '''
@@ -64,25 +51,32 @@ def select_samples(comb_stats_df, hap_df, level, sgp, n_targets):
     #subset haplotype df
     vae_hap_df = hap_df.query('sample_id in @vae_samples')
     
-    logging.info(f'Selected {len(vae_samples)} samples to be run through VAE')
+    logging.info(f'selected {len(vae_samples)} samples to be run through VAE')
 
-    return(vae_samples, vae_hap_df)
+    return vae_samples, vae_hap_df
 
 def prep_sample_kmer_table(kmers_unique_seqs, parsed_seqids):
     '''
     Prepare k-mer table for a single sample
     '''
     #set up empty arrays
-    table = np.zeros((kmers_unique_seqs.shape[0], kmers_unique_seqs.shape[2]), dtype='int')
-    n_haps = np.zeros((kmers_unique_seqs.shape[0]), dtype='int')
+    total_targets = kmers_unique_seqs.shape[0]
+    table = np.zeros((total_targets, kmers_unique_seqs.shape[2]), dtype='int')
+    n_haps = np.zeros((total_targets), dtype='int')
 
     for _, row in parsed_seqids.iterrows():
         #only record the first two haplotypes for each target
         if n_haps[row.target] < 2:
             n_haps[row.target] += 1
             table[row.target,:] += kmers_unique_seqs[row.target, row.uidx, :]
+    #double counts for homs
+    for target in np.arange(total_targets):
+        if n_haps[target] == 1:
+            table[target,:] *= 2
+    #sum over targets
+    summed_table = np.sum(table, axis=0)
 
-    
+    return summed_table
 
 def prep_kmers(vae_hap_df, vae_samples, k):
     '''
@@ -91,30 +85,18 @@ def prep_kmers(vae_hap_df, vae_samples, k):
     #translate unique sequences to k-mers
     kmers_unique_seqs = construct_unique_kmer_table(vae_hap_df, k)
     
-    logging.info('Generate k-mer tables for selected samples')
+    logging.info('generating k-mer tables for selected samples')
 
+    #set up k-mer table
+    kmers_samples = np.zeros((len(vae_samples), 4**k))
 
-    #Construct per sample kmer table
-    table = np.zeros((len(samples), 4**k), dtype='int')
-    amplified = np.zeros((len(samples), a), dtype='int')
+    #fill in table for samples
+    for n, sample in enumerate(vae_samples):
+        parsed_seqids = parse_seqids_series(vae_hap_df.loc[vae_hap_df.sample_id == sample, 
+                                                           'seqid'])
+        kmers_samples[n,:] = prep_sample_kmer_table(kmers_unique_seqs, parsed_seqids)
 
-    for e, smp in enumerate(samples):
-        smptable = np.zeros((a, 4**k))
-        smpseq = seq.loc[seq.s_Sample == smp]
-        hapcopies = np.zeros(a)
-        for r in smpseq.index:
-            combUID = smpseq.loc[r, 'combUID']
-            t, u = int(str.split(combUID, '-')[0]), int(str.split(combUID, '-')[1])
-            if hapcopies[t] < 2:
-                smptable[t,:] += kmer8[t, u, :]
-                hapcopies[t] += 1
-        smpamplified = np.where(np.isin(hapcopies,[1,2]), 1, 0)
-        for t in np.arange(a):
-            #double homozygotes
-            if hapcopies[t] == 1:
-                smptable[t,:] *= 2
-        table[e,:] = np.sum(smptable, axis=0)
-        amplified[e,:] = smpamplified
+    return(kmers_samples)
 
 
     
@@ -133,10 +115,11 @@ def vae(args):
     hap_df = pd.read_csv(args.haplotypes, sep='\t')
     comb_stats_df = pd.read_csv(args.manifest, sep='\t')
 
-    selection_criteria, version_name = prep_reference_index(args.reference_version, \
+    level, sgp, n_targets, version_name = prep_reference_index(args.reference_version, \
                                                             args.path_to_refversion)
-    vae_samples, vae_hap_df = select_samples(comb_stats_df, hap_df, selection_criteria)
-    kmer_table = prep_kmers(vae_hap_df, vae_samples, 8)
+    vae_samples, vae_hap_df = select_samples(comb_stats_df, hap_df, level, \
+                                                       sgp, n_targets)
+    kmer_table = prep_kmers(vae_hap_df, vae_samples, K)
 
 
 
@@ -157,7 +140,7 @@ def main():
                         currently a directory name. Default: gcrefv1', default='gcrefv1')
     parser.add_argument('-o', '--outdir', help='Output directory. Default: nn', default='vae')
     parser.add_argument('--path_to_refversion', help='path to reference index version.\
-         Default: test_data', default='test_data')
+         Default: ref_databases', default='ref_databases')
     parser.add_argument('--no_plotting', help='Do not generate plots. Default: False', \
                         default=False)
     parser.add_argument('-v', '--verbose', 
